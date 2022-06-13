41320679d8ccaf17313ab1e66f63177a1f88f89e
Federico Rossendy <fede07@users.noreply.github.com>
2022-06-13 00:40:17 -0300

    Completed day 3

--- Day3 - Treasure Island MiniGame/treasure-island-game.py
+++ Day3 - Treasure Island MiniGame/treasure-island-game.py
@@ -23,5 +23,22 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right"').lower()
+
+if(choice1 == "left"):
+    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
+    if(choice2 == "wait"):
+        choice3 = input("You arrive at the island umharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?").lower()
+        if(choice3 == "red"):
+            print("It's a room full of fire. Game Over.")
+        elif(choice3 == "blue"):
+            print("You found the treasure! You win!")
+        elif(choice3 == "yellow"):
+            print("You enter a room of beasts. Game Over.")
+        else:    
+            print("You choose a door that doesn't exist. Game Over.")
+    else:
+        print("You got attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
