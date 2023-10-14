d492de824f5d62fa8c775d0eb278eb7ef7bc62d8
Federico Rossendy <fede07@users.noreply.github.com>
2023-10-14 17:58:29 -0300

    Nato dictionary

---
+++ Day 26 - List Comprehension/exercise4.py
@@ -0,0 +1,9 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# 🚨 Don't change code above 👆
+# Write your code below 👇
+
+words_list = sentence.split() 
+
+result = {word:len(word) for word in words_list}
+
+print(result)
