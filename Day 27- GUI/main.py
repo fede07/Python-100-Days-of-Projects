800eaacfd6d3fb02529a5b7077abe610a9fd8f91
Federico Rossendy <fede07@users.noreply.github.com>
2023-10-14 18:22:44 -0300

    Day 27 Start

--- Day 27- GUI/main.py
+++ Day 27- GUI/main.py
@@ -4,7 +4,7 @@ window = tkinter.Tk()
 window.title("New GUI")
 window.minsize(width= 800, height=600)
 
-my_label = tkinter.Label(text="testing", font=("Arial", 24, "bold"))
+my_label = tkinter.Label(text="Testing", font=("Arial", 24, "bold"))
 my_label.pack()
 
 
