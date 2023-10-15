fb3d4861c4154afd57263491fb02809a6f300aa6
Federico Rossendy <fede07@users.noreply.github.com>
2023-10-14 22:30:52 -0300

    Playground

--- Day 27- GUI/main.py
+++ Day 27- GUI/main.py
@@ -1,12 +1,29 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("New GUI")
 window.minsize(width= 800, height=600)
 
-my_label = tkinter.Label(text="Testing", font=("Arial", 24, "bold"))
-my_label.pack()
+#label
 
+my_label = Label(text="Testing", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+my_label.config(text="New Text")
+
+#button
+
+def button_clicked():
+    print("I got clicked")
+    text_ = input.get()
+    my_label.config(text=text_)
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+#entry
+
+input = Entry(width=10)
+input.pack()
 
 
 window.mainloop()
---
+++ Day 27- GUI/playground.py
@@ -0,0 +1,12 @@
+def add(*args):
+    result = 0
+    for n in args:
+        result+=n
+    return result
+
+def calculate(**kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+
+
+
