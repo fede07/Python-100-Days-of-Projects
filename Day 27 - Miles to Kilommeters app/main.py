b339d780b0a784e5d952cd9dd7e70bc5ffcb7948
Federico Rossendy <fede07@users.noreply.github.com>
2023-10-15 16:05:16 -0300

    Completed miles to kms converter

--- Day 27 - Miles to Kilommeters app/main.py
+++ Day 27 - Miles to Kilommeters app/main.py
@@ -3,34 +3,34 @@ from tkinter import *
 FONT = ("Arial", 11)
 
 def miles_to_km(miles):
-    return miles * 1.609344
+    return miles * 1.6093
 
 def convert():
     miles = float(entry.get())
     kms = miles_to_km(miles)
-    label_11.config(text=f"{kms}")
+    result_label.config(text=f"{kms}")
 
 window = Tk()
 window.title("Mile to Km Converter")
 window.minsize(width=100, height=100)
-window.config(padx=30, pady=30)
+window.config(padx=20, pady=20)
 
-entry = Entry(width=10)
+entry = Entry(width=7)
 entry.grid(column=1, row=0)
 
-label_20 = Label(text="Miles", font=FONT)
-label_20.grid(column=2, row=0)
+miles_label = Label(text="Miles", font=FONT)
+miles_label.grid(column=2, row=0)
 
-label_01 = Label(text="is equal to", font=FONT)
-label_01.grid(column=0, row=1)
+isEqualTo_label = Label(text="is equal to", font=FONT)
+isEqualTo_label.grid(column=0, row=1)
 
-label_21 = Label(text="Km", font=FONT)
-label_21.grid(column=2, row=1)
+km_label = Label(text="Km", font=FONT)
+km_label.grid(column=2, row=1)
 
-label_11 = Label(text="", font=FONT)
-label_11.grid(column=1, row=1)
+result_label = Label(text="", font=FONT)
+result_label.grid(column=1, row=1)
 
-button = Button(text="Calculate", font=FONT, command=convert)
-button.grid(column=1, row=2)
+calculate_button = Button(text="Calculate", font=FONT, command=convert)
+calculate_button.grid(column=1, row=2)
 
 window.mainloop()
